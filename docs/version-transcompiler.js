37e5c5ba5f6ead291eea422d6de5ff153c10d666
Crementif <milanhekje@gmail.com>
2018-08-05 03:49:58 +0200

    1.14.0 transcompiler finished
    Ready to make packs now, I hope.

--- docs/version-transcompiler.js
+++ docs/version-transcompiler.js
@@ -32,6 +32,24 @@ function processTree(filename) {
 // This also supports subdirectories too, these will all still be build in the output folder.
 processTree("../");
 
+var deleteFolderRecursive = function(path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index){
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+deleteFolderRecursive("../Build");
+
+fs.mkdirSync("../Build");
+
 for (pack in graphicPacks) {
 	let instructions = {};
 	let existingExpressions = {};
@@ -62,35 +80,69 @@ for (pack in graphicPacks) {
 			currExpression.additionsLine = (currExpression.additionsLine || i)+1;
 		}
 	}
-	console.log(existingExpressions);
+	// console.log(existingExpressions);
 	
 	// Now go through all the files in the graphic pack to collect the ones it has to add to each preset.
 	for (file in graphicPacks[pack]) {
-		console.log(`${("0" + file).slice(-2)}/${("0" + (graphicPacks[pack].length-1)).slice(-2)} || Creating graphic pack "${pack}", at file "${graphicPacks[pack][file]}"...`);
-		let textContent = fs.readFileSync(pack+"/"+graphicPacks[pack][file], "utf8").split("\r\n");
-		
-		for (i=0; i<textContent.length; i++) {
-			if (textContent[i].includes("$")) {
-				for (preset in existingExpressions) {
-					for (variable in existingExpressions[preset]) {
-						if (textContent[i].includes("$"+variable)) {
-							let roughExpression = textContent[i].split("=");
-							if (roughExpression.length === 1) break;
-							expression = roughExpression[1].split(";")[0];
-							
-							if (expression.includes("+")||expression.includes("-")||expression.includes("*")||expression.includes("/")||expression.includes("(")||expression.includes(")")||expression.includes("^")) {
-								// Calculate the preset value that we need to add to the preset.
-								calc.compile(expression.replace("$","")).calc();
-								
-								//for ()
-								
-								break;
-							}
-						}
-					}
-					break; // Only check the first preset for any matches.
-				}
-			}
-		}
-	}
+        console.group(`${("0" + file).slice(-2)}/${("0" + (graphicPacks[pack].length - 1)).slice(-2)} || Creating graphic pack "${pack}", at file "${graphicPacks[pack][file]}"...`);
+        let textContent = fs.readFileSync(pack + "/" + graphicPacks[pack][file], "utf8").split("\r\n");
+        compiledGraphicPack[graphicPacks[pack][file]] = [];
+
+        for (i = 0; i < textContent.length; i++) {
+            if (textContent[i].includes("$")) {
+                for (preset in existingExpressions) {
+                    for (variable in existingExpressions[preset]) {
+                        if (textContent[i].includes("$" + variable)) {
+                            let roughExpression = textContent[i].split("=");
+                            if (roughExpression.length === 1) break;
+                            expression = roughExpression[1].split(";")[0];
+
+                            if (expression.includes("+") || expression.includes("-") || expression.includes("*") || expression.includes("/") || expression.includes("(") || expression.includes(")") || expression.includes("^")) {
+                                // Calculate the preset value that we need to add to the preset.
+                                for (preset in existingExpressions) {
+                                    console.group(`Converting ${expression.trim()} to ${expression.replace(/\$/g, "").trim()} and calculating it with these variables ${JSON.stringify(existingExpressions[preset])}.`);
+                                    let result = calc.compile(expression.replace(/\$/g, "")).calc(existingExpressions[preset]);
+                                    if (graphicPacks[pack][file] === "rules.txt") result = Math.ceil(result);
+                                    console.log(`New preset line for "${preset}" that will be added to line ${existingExpressions[preset].additionsLine} will hold ${result}.`);
+                                    let newLine = textContent[i].split("=")[0].trim() + " = $exprFile"+ file + "Line" + i + (graphicPacks[pack][file] === "rules.txt" ? "" : ";");
+                                    console.log(`New line at ${i} will be resolved to '${newLine}' and replaces '${textContent[i]}'.`);
+                                    console.groupEnd();
+
+                                    existingExpressions[preset].queue = existingExpressions[preset].queue || [];
+                                    existingExpressions[preset].queue.push("$exprFile"+ file + "Line" + i +" = "+ result);
+
+                                    if (i === compiledGraphicPack[graphicPacks[pack][file]].length) compiledGraphicPack[graphicPacks[pack][file]].push(newLine);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    break; // Only check the first preset for any matches.
+                }
+            }
+            if (i === compiledGraphicPack[graphicPacks[pack][file]].length) compiledGraphicPack[graphicPacks[pack][file]].push(textContent[i]);
+        }
+        console.groupEnd();
+    }
+    lineOffset = 0;
+    for (preset in existingExpressions) {
+	    let additionalLines = [];
+	    if (existingExpressions[preset].hasOwnProperty("queue")) {
+            additionalLines[existingExpressions[preset].additionsLine] = [];
+	        for (queueItem in existingExpressions[preset].queue) {
+	            additionalLines[existingExpressions[preset].additionsLine].push(existingExpressions[preset].queue[queueItem]);
+            }
+        }
+        for (newLine in additionalLines) {
+	        compiledGraphicPack["rules.txt"].splice(parseInt(newLine)+lineOffset, 0, additionalLines[newLine].join("\r\n"));
+	        lineOffset++;
+        }
+        let newRulesFile = [];
+    }
+
+    fs.mkdirSync("../Build/"+pack.split("/")[pack.split("/").length-1]);
+    for (file in compiledGraphicPack) {
+        fs.writeFileSync("../Build/"+pack.split("/")[pack.split("/").length-1]+"/"+file, compiledGraphicPack[file].join("\r\n"));
+    }
+    //console.log("", compiledGraphicPack["rules.txt"]);
 }
